dccfdd8a6dc73c42fe501f55b808028438b4c154
Peter Jeffery <mr__j@hotmail.com>
2012-03-29 09:23:40 +0000

    Start of IMAP protocol, logs in, lists folders, selects inbox.

---
+++ AccountInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mail
+{
+    public class AccountInfo
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public int Username { get; set; }
+        public int Password { get; set; }        
+    }
+}
---
+++ Imap.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Collections.ObjectModel;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Mail
+{
+    public enum ImapState
+    {
+        None,
+        Connected,
+        LoggedIn,
+        Selected
+    }
+
+    public class Imap
+    {
+        private AccountInfo account_;
+        private TcpClient client_;        
+
+        private byte[] incoming_;
+
+        private ImapState state_;
+        private int cmdId_ = 0;
+        private Dictionary<string, string> pendingCommands_;
+
+        private ObservableCollection<string> folders_;
+
+        public Imap(AccountInfo account)
+        {
+            account_ = account;
+            state_ = ImapState.None;
+            pendingCommands_ = new Dictionary<string, string>();
+
+            folders_ = new ObservableCollection<string>();
+
+            client_ = new TcpClient(account.Host, account.Port);
+
+            if (client_.Connected)
+            {
+                state_ = ImapState.Connected;
+
+                incoming_ = new byte[client_.Available];
+                client_.GetStream().BeginRead(incoming_, 0, incoming_.Length, HandleRead, null);
+            }
+        }
+
+        void HandleRead(IAsyncResult res)
+        {
+            int bytesRead = client_.GetStream().EndRead(res);
+
+            if (bytesRead > 0)
+            {
+                string response = UTF8Encoding.UTF8.GetString(incoming_, 0, bytesRead);
+
+                ProcessResponse(response);
+            }
+
+            incoming_ = new byte[client_.Available];
+            client_.GetStream().BeginRead(incoming_, 0, incoming_.Length, HandleRead, null);
+        }
+
+        void ProcessResponse(string responseText)
+        {
+            System.Diagnostics.Debug.Write(responseText);
+
+            string[] responses = responseText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var responseLine in responses)
+            {
+                string responseTo = null;
+                string result = null;
+                string resultData = null;
+                bool commandComplete = false;
+
+                string[] responseData = responseLine.Split(new char[] { ' ' });
+                if (responseData[0] == "*")
+                {
+                    if (state_ == ImapState.Connected)
+                    {
+                        result = responseData[1];
+                    }
+                    else
+                    {
+                        responseTo = responseData[1];
+                        result = "OK";
+                    }
+
+                    resultData = string.Join(" ", responseData, 2, responseData.Length - 2);
+                }
+                else
+                {
+                    // match it to the request command of this name.
+                    string sentCommand = pendingCommands_[responseData[0]];
+                    string[] command = sentCommand.Split(new char[] { ' ' });
+
+                    responseTo = command[0];
+                    int resultOffset = 2;
+                    if (responseData[resultOffset] != responseTo)
+                    {
+                        --resultOffset;
+                    }
+                    
+                    result = responseData[resultOffset];
+                    resultData = string.Join(" ", responseData, resultOffset + 1, responseData.Length - resultOffset - 1);
+                    commandComplete = true;
+
+                    pendingCommands_.Remove(responseData[0]);
+                }
+
+                if (result == "OK")
+                {
+                    switch (state_)
+                    {
+                        case ImapState.Connected:
+                            if (responseTo == null)
+                            {
+                                Login();
+                            }
+                            else if (responseTo == "LOGIN")
+                            {
+                                state_ = ImapState.LoggedIn;
+
+                                ListFolders();
+                            }
+                            break;
+
+                        case ImapState.LoggedIn:
+                            if (responseTo == "LSUB")
+                            {
+                                if (commandComplete)
+                                {
+                                    SelectFolder(folders_[0]);
+                                }
+                                else
+                                {
+                                    ListedFolder(resultData);
+                                }
+                            }
+                            else if (responseTo == "SELECT")
+                            {
+                                state_ = ImapState.Selected;
+                                ListMessages();
+                            }
+                            break;
+
+                        case ImapState.Selected:
+                            if (responseTo == "SEARCH")
+                            {
+                                AvailableMessages(resultData);
+                            }
+                            break;
+                    }
+                }
+                else if (result == "NO")
+                {
+                }
+                else if (result == "BAD")
+                {
+                }
+            }
+        }
+
+        string NextCommand()
+        {
+            lock (this)
+            {
+                ++cmdId_;
+
+                return string.Format("A{0:D4}", cmdId_);
+            }
+        }
+
+        void SendCommand(string command)
+        {
+            string commandId = NextCommand();
+            string cmd = commandId + " " + command + "\r\n";
+
+            pendingCommands_[commandId] = command;
+
+            byte[] bytes = UTF8Encoding.UTF8.GetBytes(cmd);
+
+            client_.GetStream().Write(bytes, 0, bytes.Length);
+        }
+
+        void Login()
+        {
+            string command = "LOGIN " + account_.Username + " " + account_.Password;
+
+            SendCommand(command);
+        }
+
+        void ListFolders()
+        {
+            folders_.Clear();
+
+            string command = "LSUB " + "\"\" \"*\"";
+            SendCommand(command);
+        }
+
+        void ListedFolder(string responseData)
+        {
+            string[] data = responseData.Split(new char[] { '\"' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string flags = data[0].Trim();
+            flags = flags.Substring(1, flags.Length - 2);
+
+            string nameSpace = data[1];
+            string folder = data[3];
+
+            folders_.Add(folder);
+        }
+
+        void SelectFolder(string folderName)
+        {
+            SendCommand("SELECT " + folderName);
+        }
+
+        void ListMessages()
+        {
+            SendCommand("SEARCH ALL");
+        }
+
+        void AvailableMessages(string responseData)
+        {
+            
+        }
+    }
+}
