87f1c0b88aec4fa0c5697c3cb8991994028b2515
Peter Jeffery <mr__j@hotmail.com>
2012-04-20 14:50:26 +0000

    Start of dialog for creating accounts

---
+++ AddAccount.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mail
+{
+    /// <summary>
+    /// Interaction logic for AddAccount.xaml
+    /// </summary>
+    public partial class AddAccount: Window
+    {
+        public AddAccount()
+        {
+            InitializeComponent();
+        }
+
+        public void spam()
+        {
+            byte[] password = null;
+            System.Security.Cryptography.ProtectedData.Protect(password);
+        }
+    }
+}
