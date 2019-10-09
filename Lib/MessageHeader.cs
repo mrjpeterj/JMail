198777c6179770a01f9d8d4c60d24f5970b459f8
Peter Jeffery <mr__j@hotmail.com>
2019-10-09 11:41:22 +0100

    remove unused preprocessor check

--- Lib/MessageHeader.cs
+++ Lib/MessageHeader.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using System.ComponentModel;
-
-#if !NETFX_CORE
+using System.Linq;
 using System.Net.Mail;
-#endif
+using System.Text;
 
 namespace JMail
 {
