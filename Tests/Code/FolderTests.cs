d2e23ae8b2c627f51ec422df9dcf47045b7c882c
Peter Jeffery <mr__j@hotmail.com>
2019-10-14 16:28:58 +0100

    start tests for folder and message behavior

---
+++ Tests/Code/FolderTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+using JMail.Core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JMail
+{
+    [TestClass]
+    public class FolderTests
+    {
+        TestAccount testServer_;
+        IAccount server_;
+
+        [TestInitialize]
+        public void Init()
+        {
+            testServer_ = new TestAccount();
+
+            server_ = testServer_;
+            server_.Connect();
+        }
+
+        [TestCleanup]
+        public void Shutdown()
+        {
+            server_.Shutdown();
+            server_ = null;
+        }
+
+        [TestMethod]
+        public void MessagesArrive()
+        {
+            int folderMsgCount = -1;
+            int folderUnreadCount = -1;
+
+            int msgCount = -1;
+
+            // Wait for the inbox to appear
+            var findInbox = server_.AllFolders.Select((folders) =>
+            {
+                return folders.Where(folder => folder.FullName == "INBOX").FirstOrDefault();
+            })
+                .Where(folder => folder != null)
+                .Take(1);
+
+            // Now subscribe to interesting parts of the inbox
+            findInbox.Subscribe(folder =>
+            {
+                server_.SelectFolder(folder);
+
+                folder.Exists.Subscribe(val => folderMsgCount = val);
+                folder.Unseen.Subscribe(val => folderUnreadCount = val);
+
+                folder.Messages.Subscribe(msgs =>
+                {
+                    msgCount = msgs.Count();
+                });
+            });
+
+            // Now start feeding the server with fake data.
+
+            // Get the Inbox to appear
+            var inbox = new Folder(server_, "INBOX", "INBOX", ".", false, true);
+            testServer_.AddFolder(inbox);
+
+            // Add a message to it.
+            var msg1 = new MessageHeader(1, inbox);
+            inbox.AddMessage(msg1);
+
+            Assert.AreEqual(1, folderMsgCount);
+            Assert.AreEqual(1, folderUnreadCount);
+            Assert.AreEqual(1, msgCount);
+
+            // Add another message to it.
+            var msg2 = new MessageHeader(2, inbox);
+            inbox.AddMessage(msg2);
+
+            Assert.AreEqual(2, folderMsgCount);
+            Assert.AreEqual(2, folderUnreadCount);
+            Assert.AreEqual(2, msgCount);
+
+            // Mark the first message as read
+            server_.SetFlag(msg1, MessageFlags.Seen, true);
+
+            Assert.AreEqual(2, folderMsgCount);
+            Assert.AreEqual(1, folderUnreadCount);
+            Assert.AreEqual(2, msgCount);
+
+            // Delete the 2nd message
+            server_.SetFlag(msg2, MessageFlags.Deleted, true);
+
+            Assert.AreEqual(2, folderMsgCount);
+            Assert.AreEqual(1, folderUnreadCount);
+            Assert.AreEqual(2, msgCount);
+
+            // Expunge the folder
+            server_.ExpungeFolder();
+
+            Assert.AreEqual(1, folderMsgCount);
+            Assert.AreEqual(0, folderUnreadCount);
+            Assert.AreEqual(1, msgCount);
+        }
+    }
+}
---
+++ Tests/Code/TestAccount.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+using JMail.Core;
+
+namespace JMail
+{
+    class TestAccount: IAccount
+    {
+        public event EventHandler AuthFailed;
+
+        private List<Folder> allFoldersList_;
+        private List<Folder> topFoldersList_;
+
+        private BehaviorSubject<IEnumerable<Folder>> allFolders_;
+        private BehaviorSubject<IEnumerable<Folder>> topFolders_;
+
+        private Folder currentFolder_;
+
+        public IObservable<IEnumerable<Folder>> FolderList { get { return topFolders_; } }
+        public IObservable<IEnumerable<Folder>> AllFolders { get { return allFolders_; } }
+
+        public TestAccount()
+        {
+            allFoldersList_ = new List<Folder>();
+            allFolders_ = new BehaviorSubject<IEnumerable<Folder>>(allFoldersList_);
+
+            topFoldersList_ = new List<Folder>();
+            topFolders_ = new BehaviorSubject<IEnumerable<Folder>>(topFoldersList_);
+        }
+
+        public void SelectFolder(Folder f)
+        {
+            currentFolder_ = f;
+        }
+
+        public void UnselectFolder(Folder f)
+        {
+            if (currentFolder_ == f)
+            {
+                currentFolder_ = null;
+            }
+        }
+
+        public void RenameFolder(string oldName, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeFolder(string folderName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FetchMessage(MessageHeader m, BodyPart p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFlag(MessageHeader m, MessageFlags flags, bool isSet)
+        {
+            m.SetFlag(flags, isSet);
+        }
+
+        public void ExpungeFolder()
+        {
+            if (currentFolder_ != null)
+            {
+                currentFolder_.Messages.Take(1).Subscribe(msgs =>
+                {
+                    var deleted = msgs.Where(msg => msg.IsDeleted).ToList();
+
+                    foreach (var d in deleted)
+                    {
+                        currentFolder_.RemoveMessage(d);
+                    }
+                });
+            }
+        }
+
+        public void SearchFolder(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchEnd()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect()
+        {
+            
+        }
+
+        public void PollFolders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            
+        }
+
+        ///// Impl methods for controlling the data 
+        public void AddFolder(Folder f)
+        {
+            allFoldersList_.Add(f);
+            allFolders_.OnNext(allFoldersList_);
+        }
+    }
+}
